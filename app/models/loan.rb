c62bd78ee80183fc9215fcd272c199c7c2dc155e
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-10 20:39:31 -0400

    add remaining balance migration and update update loans controller spec to include remaining balance update seed data to include remaining balanace

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,6 +1,6 @@
 class Loan < ActiveRecord::Base
   has_many :payments
 
-  validates :amount_funded, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
-  
+  validates :funded_amount, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :remaining_balance, presence: true, numericality: true
 end
