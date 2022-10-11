545382e15643477477c6657b3a3221754ab20fe3
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-10 20:40:39 -0400

    tweak validation to ensure balance is not overpaid via validation

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,5 +2,5 @@ class Loan < ActiveRecord::Base
   has_many :payments
 
   validates :funded_amount, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :remaining_balance, presence: true, numericality: true
+  validates :remaining_balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
