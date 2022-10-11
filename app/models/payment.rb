c823e4f3256c60f25a1e01abb2c8767e2c0c7eaa
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-10 20:20:44 -0400

    add model validations and tests for payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,6 @@ class Payment < ActiveRecord::Base
   belongs_to :loan
 
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :date, presence: true
+  validates :loan_id, presence: true
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe Payment, type: :model do
     it { should belong_to(:loan) }
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:amount) }
+    it { should validate_presence_of(:date) }
+    it { should validate_numericality_of(:amount) }
+    it { should validate_presence_of(:loan_id) }
+  end
   describe 'instance methods' do
     it '#finds total amount of payments' do
-      
+
     end
   end
 
